55d2f9a6aa0d921eec0a4c4d900197713b58739a
Kota Kakiuchi <kakiuchi@crimson.q.t.u-tokyo.ac.jp>
2017-12-28 20:56:17 +0900

    update for practical use

--- dataset/get_data.sh
+++ dataset/get_data.sh
@@ -1,4 +1,10 @@
 # directory for snow images
-wget "<URL>" -O <file name>
+wget "https://www.dropbox.com/s/sdk1en3o0eli697/original.zip" -O original.zip
 # directory for original images
-wget "<URL>" -O <file name>
+wget "https://www.dropbox.com/s/yjnwky1kwlaioyb/snow.zip" -O snow.zip
+
+# unzip
+unzip original.zip
+unzip snow.zip
+rm original.zip
+rm snow.zip
